1a633de25db6a6aa763721a8f38559be7a68a750
windowsnipz <nstrickland2014@gmail.com>
2024-12-31 19:50:36 -0500

    moved things around because of scope

--- script.js
+++ script.js
@@ -2,31 +2,32 @@ const quoteBtn = document.getElementById('quote-btn');
 const outputDiv = document.getElementById('output');
 const resultText = document.getElementById('result-text');
 
+// Creates an array of string names to represent the tiles and number of each tile
+const tiles = [
+    ...Array(4).fill("Wood"),
+    ...Array(3).fill("Brick"),
+    ...Array(4).fill("Sheep"),
+    ...Array(4).fill("Wheat"),
+    ...Array(3).fill("Ore"),
+    ...Array(1).fill("Robber")
+]
+
+// This is the Fisher-Yates shuffle algorithm, shuffling the array
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array [j], array[i]];
+    }
+}
+
+// Shuffle the tiles
+shuffle(tiles);
+
 
 quoteBtn.addEventListener('click', () => {
 
-    // Creates an array of string names to represent the tiles and number of each tile
-    const tiles = [
-        ...Array(4).fill("Wood"),
-        ...Array(3).fill("Brick"),
-        ...Array(4).fill("Sheep"),
-        ...Array(4).fill("Wheat"),
-        ...Array(3).fill("Ore"),
-        ...Array(1).fill("Robber")
-    ]
 
-    // This is the Fisher-Yates shuffle algorithm, shuffling the array
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array [j], array[i]];
-        }
-    }
-
-    // Shuffle the tiles
-    shuffle(tiles);
-
-    
+    // Display
 
 
 
