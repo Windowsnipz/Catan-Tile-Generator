cf44ce6690d0c0fc10b6eb4d4894456b177fabfa
windowsnipz <nstrickland2014@gmail.com>
2024-12-31 20:01:09 -0500

    some functional progress

--- script.js
+++ script.js
@@ -36,26 +36,4 @@ function displayNextTile() {
 }
 
 
-quoteBtn.addEventListener('click', () => {
-
-
-    fetch('https://api.kanye.rest/')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json(); // Parses JSON data
-        })
-        .then(data => {
-            outputDiv.classList.add('section');
-            resultText.textContent = data.quote;  // Display Kanye quote in result section
-        })
-        .catch (error => {
-            console.error('There was a problem with the fetch operation');
-        });
-});
-
-document.body.addEventListener('click', (event) => {
-        outputDiv.classList.remove('section');
-        resultText.textContent = '';
-});
+quoteBtn.addEventListener('click', displayNextTile);
